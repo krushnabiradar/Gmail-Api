f0013869005258954ebd87ccee95f94c30d6c7f3
krushnabiradar <krushnabiradar1995@.com>
2023-11-07 12:28:13 +0530

    create lable function

--- index.js
+++ index.js
@@ -13,6 +13,57 @@ const SCOPES = [
 ];
 const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
 
+//  function to get unreplied mails/messages
+async function unRepliedMessages(auth, gmail) {
+  const response = await gmail.users.messages.list({
+    userId: "me",
+    labelIds: ["INBOX"],
+    q: "is:unread",
+  });
+  return response.data.messages || [];
+}
+
+// Function to create new lable for auto reply messages
+const labelName = "Auto-Reply";
+async function createLabel(auth, gmail) {
+  try {
+    const response = await gmail.users.labels.create({
+      userId: "me",
+      requestBody: {
+        name: labelName,
+        labelListVisibility: "labelShow",
+        messageListVisibility: "show",
+      },
+    });
+    return response.data.id;
+  } catch (error) {
+    if (error.code === 409) {
+      const response = await gmail.users.labels.list({
+        userId: "me",
+      });
+      const label = response.data.labels.find(
+        (label) => label.name === labelName
+      );
+      return label.id;
+    } else {
+      throw error;
+    }
+  }
+}
+
+
+//  Function to create Mail and auto send reply
+
+
+
+//  main function to call all function 
+async function mainLogic(auth, gmail) {
+  const labelId = await createLabel(auth, gmail);
+
+//    Set time interval for cheking and sending mail every 45-120 sec
+ 
+}
+
 app.get("/", async (req, res) => {
   try {
     const auth = await authenticate({
@@ -21,6 +72,9 @@ app.get("/", async (req, res) => {
     });
 
     const gmail = google.gmail({ version: "v1", auth });
+
+    // Call the main logic function
+    await mainLogic(auth, gmail);
 
     res.json({ Auth: auth });
   } catch (error) {
